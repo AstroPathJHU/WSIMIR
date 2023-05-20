96080c26ec91b6a94ca76f39e0e6e90314326c7b
Ben Green <46007058+beng1290@users.noreply.github.com>
2023-05-20 03:38:00 -0400

    support for empty control images
    Since control images are selected randomly they could potentially be completely empty. In that case NaNs were introduced during image scaling.

---
+++ zero_and_scale.m
@@ -0,0 +1,21 @@
+%% zero_and_scale
+%% Description
+% perform a zeroing then scaling of the images
+%%
+%
+function zs = zero_and_scale(img, sc)
+%
+[h, w, bands] = size(img);
+data = reshape(img, [], bands);
+%
+mincol = min(data,[],1);
+zs = data - mincol;
+%
+maxcol = max(zs,[],1);
+scale = sc ./ cast((maxcol), 'single');
+%
+zs = cast(zs, 'single') .* scale;
+zs(isnan(zs)) = 0;
+zs = reshape(zs, h, w, []);
+%
+end
